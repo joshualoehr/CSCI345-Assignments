59991250c9cde22d6927e68dc5e8ce3f1fbfca45
Dayne Andersen <Dayandersen@gmail.com>
2017-05-03 12:25:44 -0700

    I HELPED

--- src/csci345/Player.java
+++ src/csci345/Player.java
@@ -2,4 +2,21 @@ package csci345;
 
 public class Player {
 
+    private String name;
+    private Room currRoom;
+    private Role role;
+    private int rank;
+    private int dollars;
+    private int credits;
+    private int rehearsalChips;
+
+    public void move() {}
+
+    public void rehearse() {}
+
+    public void act() {}
+
+    public void upgrade() {}
+
+    public void takeRole() {}
 }
