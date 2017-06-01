dd4820ddaa061cd088199cac937e87ed7e5186fe
Dayne Andersen <Dayandersen@gmail.com>
2017-06-01 15:43:52 -0700

    added points

--- src/controller/Room.java
+++ src/controller/Room.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.awt.Rectangle;
+import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -29,6 +30,24 @@ public class Room extends JLayeredPane {
 		roomBounds.put("Ranch", new Rectangle(230, 460, 370, 220));
 		roomBounds.put("Casting Office", new Rectangle(10, 460, 205, 200));
 		roomBounds.put("Secret Hideout", new Rectangle(10, 685, 590, 205));
+	}
+	
+	private static HashMap<String, Point> playerHolding;
+	static {
+		playerHolding = new HashMap<String,Point>();
+		playerHolding.put("Train Station", new Point(20,230));
+		playerHolding.put("Jail", new Point(400,210));
+		playerHolding.put("General Store", new Point(290,400));
+		playerHolding.put("Main Street", new Point(800,100));
+		playerHolding.put("Trailers", new Point(1000,300));
+		playerHolding.put("Saloon", new Point(780,400));
+		playerHolding.put("Bank", new Point(760,600));
+		playerHolding.put("Church", new Point(750,700));
+		playerHolding.put("Hotel", new Point(1000,475));
+		playerHolding.put("Ranch", new Point(280,650));
+		playerHolding.put("Casting Office", new Point(20,625));
+		playerHolding.put("Secret Hideout", new Point(260,850));
+		
 	}
 	
 	private JLabel clickArea;
